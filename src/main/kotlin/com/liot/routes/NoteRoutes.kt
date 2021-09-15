5b7a8cb5a9450ddb53c2871a41a641f8fa9dd9ce
Lio <liomazza@gmail.com>
2021-09-15 14:47:42 -0300

    add & update note

--- src/main/kotlin/com/liot/routes/NoteRoutes.kt
+++ src/main/kotlin/com/liot/routes/NoteRoutes.kt
@@ -1,9 +1,14 @@
 package com.liot.routes
 
+import com.liot.data.collections.Note
 import com.liot.data.getNotesForUser
+import com.liot.data.saveNote
 import io.ktor.application.*
 import io.ktor.auth.*
+import io.ktor.http.HttpStatusCode.Companion.BadRequest
+import io.ktor.http.HttpStatusCode.Companion.Conflict
 import io.ktor.http.HttpStatusCode.Companion.OK
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
@@ -14,6 +19,25 @@ fun Route.noteRoutes() {
                 val email = call.principal<UserIdPrincipal>()!!.name
                 val notes = getNotesForUser(email)
                 call.respond(OK, notes)
+            }
+        }
+    }
+
+    route("/addnote") {
+        authenticate {
+            post {
+                val note = try {
+                   call.receive<Note>()
+                } catch (e: ContentTransformationException){
+                    call.respond(BadRequest)
+                    return@post
+                }
+                if(saveNote(note)) {
+                    call.respond(OK)
+                } else {
+                    call.respond(Conflict)
+                }
+
             }
         }
     }
