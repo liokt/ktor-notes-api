3d14e3680fe3535def02273923340a3bb5e3bfb3
Lio <liomazza@gmail.com>
2021-09-15 07:50:15 -0300

    authenticating user

--- src/main/kotlin/com/liot/Application.kt
+++ src/main/kotlin/com/liot/Application.kt
@@ -1,12 +1,15 @@
 package com.liot
 
+import com.liot.data.checkPasswordForEmail
 import com.liot.plugins.configureRouting
 import com.liot.routes.loginRoute
 import com.liot.routes.registerRoute
 import io.ktor.application.*
+import io.ktor.auth.*
 import io.ktor.features.*
 import io.ktor.gson.*
 import io.ktor.routing.*
+import java.nio.file.attribute.UserPrincipal
 
 fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
@@ -25,5 +28,23 @@ fun Application.module() {
         }
     }
 
+    install(Authentication) {
+        configureAuth()
+    }
+
     configureRouting()
+}
+
+
+private fun Authentication.Configuration.configureAuth() {
+    basic {
+        realm = "Note Server"
+        validate { credentials ->
+            val email = credentials.name
+            val password = credentials.password
+            if (checkPasswordForEmail(email, password)) {
+                UserIdPrincipal(email)
+            } else null
+        }
+    }
 }
